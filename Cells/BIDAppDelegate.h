20ce00c20b7ca1807be1ea576527808913bbf62a
JMac <sysadminktp@mail.ru>
2013-02-28 22:33:52 +0400

    no message

---
+++ Cells/BIDAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  BIDAppDelegate.h
+//  Cells
+//
+//  Created by JMac on 28.02.13.
+//  Copyright (c) 2013 JMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BIDViewController;
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) BIDViewController *viewController;
+
+@end
---
+++ Cells/BIDNameAndColorCell.h
@@ -0,0 +1,13 @@
+//
+//  BIDNameAndColorCell.h
+//  Cells
+//
+//  Created by JMac on 28.02.13.
+//  Copyright (c) 2013 JMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDNameAndColorCell : UITableViewCell
+
+@end
